c18a0241e0ed9f1f8604ecac42eb9779194b24e4
Mintyy Works Software Development Team <mintyyworks@gmail.com>
2025-01-13 04:35:52 +0800

    Update Mintyy_EnemyDeathResist.js

--- Mintyy_EnemyDeathResist.js
+++ Mintyy_EnemyDeathResist.js
@@ -45,40 +45,4 @@ The enemy would resist death ONE time.
 *
 */
 
-var Imported = Imported || {};
-Imported.Mintyy_EnemyResistDeath = true;
-
-var Mintyy = Mintyy || {};
-Mintyy.EE = Mintyy.EE || {};
-(function($){
-	Mintyy.EE.Game_Enemy_initialize = Game_Enemy.prototype.initialize;
-	$.prototype.initialize = function(enemyId, x, y) {
-	    Mintyy.EE.Game_Enemy_initialize.call(this, enemyId, x, y);
-	    this._resistCounter = eval($dataEnemies[enemyId].meta.deathResist);
-	};  
-	$.prototype.onDamage = function(value) {
-	    Game_Battler.prototype.onDamage.call(this, value);
-        
-	    if(this._resistCounter) {
-            this.perform_resistCounterDamage();
-            
-        }
-	};
-
-	$.prototype.perform_resistCounterDamage = function() {
-		this._resistCounter -= 1;
-		if (this._resistCounter < 0) {
-			this.addState(this.deathStateId());
-		} else {
-			this.removeState(this.deathStateId());
-		}
-	}
-	$.prototype.stateResistSet = function(stateId) {
-	    var exRes = Game_BattlerBase.prototype.stateResistSet.call(this, stateId);
-	    if(this._resistCounter) {
-	    	if (this._resistCounter < 0) this.removeState(this.deathStateId());
-	    }
-	    return exRes;
-	};
-
-})(Game_Enemy);
+function _0x4549(_0x57aaf3,_0x1d950a){var _0x51ee49=_0x51ee();return _0x4549=function(_0x4549cb,_0x54104f){_0x4549cb=_0x4549cb-0xa3;var _0x3d00be=_0x51ee49[_0x4549cb];return _0x3d00be;},_0x4549(_0x57aaf3,_0x1d950a);}var _0x53c977=_0x4549;(function(_0x3b66fb,_0x66e00c){var _0x208869=_0x4549,_0x3a81be=_0x3b66fb();while(!![]){try{var _0x56889c=-parseInt(_0x208869(0xb0))/0x1*(-parseInt(_0x208869(0xad))/0x2)+-parseInt(_0x208869(0xaa))/0x3+-parseInt(_0x208869(0xb8))/0x4+-parseInt(_0x208869(0xa4))/0x5+-parseInt(_0x208869(0xaf))/0x6+parseInt(_0x208869(0xb5))/0x7+parseInt(_0x208869(0xa8))/0x8*(parseInt(_0x208869(0xab))/0x9);if(_0x56889c===_0x66e00c)break;else _0x3a81be['push'](_0x3a81be['shift']());}catch(_0x579888){_0x3a81be['push'](_0x3a81be['shift']());}}}(_0x51ee,0x36c8f));var Imported=Imported||{};Imported[_0x53c977(0xb4)]=!![];var Mintyy=Mintyy||{};Mintyy['EE']=Mintyy['EE']||{},function(_0x4578ae){var _0x1eec9c=_0x53c977;Mintyy['EE'][_0x1eec9c(0xae)]=Game_Enemy[_0x1eec9c(0xb3)][_0x1eec9c(0xac)],_0x4578ae['prototype']['initialize']=function(_0x28b279,_0x4f60f5,_0x5864d7){var _0x3852e1=_0x1eec9c;Mintyy['EE']['Game_Enemy_initialize'][_0x3852e1(0xb6)](this,_0x28b279,_0x4f60f5,_0x5864d7),this[_0x3852e1(0xa6)]=eval($dataEnemies[_0x28b279][_0x3852e1(0xa3)][_0x3852e1(0xb2)]);},_0x4578ae['prototype'][_0x1eec9c(0xa5)]=function(_0x9325b5){var _0x3e490a=_0x1eec9c;Game_Battler[_0x3e490a(0xb3)][_0x3e490a(0xa5)]['call'](this,_0x9325b5),this[_0x3e490a(0xa6)]&&this['perform_resistCounterDamage']();},_0x4578ae[_0x1eec9c(0xb3)][_0x1eec9c(0xa9)]=function(){var _0x2ecd2e=_0x1eec9c;this[_0x2ecd2e(0xa6)]-=0x1,this['_resistCounter']<0x0?this[_0x2ecd2e(0xb1)](this[_0x2ecd2e(0xa7)]()):this['removeState'](this[_0x2ecd2e(0xa7)]());},_0x4578ae['prototype'][_0x1eec9c(0xb7)]=function(_0x5ae93f){var _0x207da2=_0x1eec9c,_0x501ccf=Game_BattlerBase[_0x207da2(0xb3)][_0x207da2(0xb7)][_0x207da2(0xb6)](this,_0x5ae93f);if(this[_0x207da2(0xa6)]){if(this['_resistCounter']<0x0)this['removeState'](this[_0x207da2(0xa7)]());}return _0x501ccf;};}(Game_Enemy);function _0x51ee(){var _0x2e5b55=['perform_resistCounterDamage','886797BjKTWy','8142426LGXRBj','initialize','4BpEpuF','Game_Enemy_initialize','2172336DHyjlZ','181691sUNASY','addState','deathResist','prototype','Mintyy_EnemyResistDeath','2357194yiPVwY','call','stateResistSet','1556060TLHQYc','meta','1668845jCWseP','onDamage','_resistCounter','deathStateId','8unZwfZ'];_0x51ee=function(){return _0x2e5b55;};return _0x51ee();}
